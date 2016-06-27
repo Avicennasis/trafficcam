39f90d94d5cd15f3e41023d436792a3fba81d715
Avic <Avicennasis@gmail.com>
2016-06-27 03:05:27 -0400

    Update trafficcam.sh
    expand commands, more verbose, tests if file is empty or not

--- trafficcam.sh
+++ trafficcam.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-for i in {1..10}; do curl -o ~/TEMP/cam.jpg http://link.to/camimage.jpg ; mpack -s TrafficCam -a ~/TEMP/cam.jpg USERNAME@gmail.com ; rm ~/TEMP/cam.jpg ; sleep 60 ; done
+#Make the directory for the temporary file storage if it doesn't exist yet
+if [ ! -d ${HOME}/TEMP ]; then
+        echo "TEMP directory not found! Creating it now"
+        mkdir -p "${HOME}/TEMP"
+fi
+
+#The actual work 
+for i in {1..10}; do
+
+curl -o ${HOME}/TEMP/cam.jpg http://link.to/camimage.jpg
+if [[ -s ${HOME}/TEMP/cam.jpg ]] ; then
+echo "We have the image."
+echo "Sending email now!"
+mpack -s TrafficCam -a ${HOME}/TEMP/cam.jpg USERNAME@gmail.com
+echo "Email sent!"
+rm ${HOME}/TEMP/cam.jpg
+sleep 60
+else
+echo "Whoops, lemme retry that..."
+rm ${HOME}/TEMP/cam.jpg
+sleep 60
+fi
+
+done
