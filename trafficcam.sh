2c10e07172658e3726c2a2ac66bd471c675b16e8
Avic <Avicennasis@gmail.com>
2017-03-27 22:50:47 -0400

    Update trafficcam.sh
    Double quotes to prevent globbing and word splitting.

--- trafficcam.sh
+++ trafficcam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Make the directory for the temporary file storage if it doesn't exist yet
-if [ ! -d ${HOME}/TEMP ]; then
+if [ ! -d "${HOME}/TEMP" ]; then
         echo "TEMP directory not found! Creating it now"
         mkdir -p "${HOME}/TEMP"
 fi
@@ -9,17 +9,17 @@ fi
 #The actual work 
 for i in {1..10}; do
 
-curl -o ${HOME}/TEMP/cam.jpg http://link.to/camimage.jpg
+curl -o "${HOME}/TEMP/cam.jpg http://link.to/camimage.jpg"
 if [[ -s ${HOME}/TEMP/cam.jpg ]] ; then
 echo "We have the image."
 echo "Sending email now!"
-mpack -s TrafficCam -a ${HOME}/TEMP/cam.jpg USERNAME@gmail.com
+mpack -s TrafficCam -a "${HOME}/TEMP/cam.jpg" USERNAME@gmail.com
 echo "Email sent!"
-rm ${HOME}/TEMP/cam.jpg
+rm "${HOME}/TEMP/cam.jpg"
 sleep 60
 else
 echo "Whoops, lemme retry that..."
-rm ${HOME}/TEMP/cam.jpg
+rm "${HOME}/TEMP/cam.jpg"
 sleep 60
 fi
 
